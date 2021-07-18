82b8e7d2cc03b4fc1458d2e5d7fd62d5b69f5236
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-07-17 23:44:14 -0700

    Update Canvas.js

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,7 +4,7 @@ import "./Canvas.css";
 function Canvas() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-  
+
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = window.innerWidth;
@@ -12,43 +12,56 @@ function Canvas() {
     const ctx = canvas.getContext("2d");
     const g = 980;
     
-    var ball = new Ball(canvas.width/2, canvas.height/2, 70, 'blue');
+    canvas.addEventListener('click', (e) => {
+      const pos = {
+        x: e.clientX,
+        y: e.clientY
+      };
+
+      console.log(pos);
+      
+    });  
+
+    var ball = new Ball(canvas.width/2, 0, 70, 'blue', 0.555);
     
     ball.draw();
     animate();
     
-
     contextRef.current = ctx;
     
-    function Ball(x, y, radius, color) {
+    function Ball(x, y, radius, color, elasticity) {
       this.x = x;
       this.y = y;
       this.vy = 0;
+      this.vx = 0;
+      this.offsetX = 0;
+      this.offsetY = 0;
+      this.elasticity = elasticity;
+      this.isOut = false;
       this.radius = radius;
       this.color = color;
+      this.isDragging = false;
   
       this.fall = function() {
         if (this.y + this.radius >= canvas.height) {
-          if (Math.abs(this.vy) <= 0.00001) {
+          if (Math.abs(this.vy) <= 0.1) {
             this.vy = 0;
             this.y = canvas.height - radius;
           }
-          if (Math.abs(this.vy) > 0.00001) {
-            console.log(" hit : " , Math.abs(this.vy));
-            this.calculateVy('hit');
+          if (Math.abs(this.vy) > 0.1) {
+            this.calculateVy(this.elasticity, 'hit');
             this.calculateY();
           }
         } else {
-          this.calculateVy();
+          this.calculateVy(this.elasticity);
           this.calculateY();
         }
         this.draw();
       };
 
-      this.calculateVy = function(direction) {
+      this.calculateVy = function(elasticity = 1.0, direction = '') {
         if (direction === "hit") {
-          this.vy = -this.vy * 0.95;
-          console.log("hit");
+          this.vy = -this.vy * elasticity;
         } else {
           this.vy += g/60;
         }
@@ -56,6 +69,9 @@ function Canvas() {
 
       this.calculateY = function() {
         this.y += (this.vy/60) + g/7200;
+        if (this.y + radius >= canvas.height) {
+          this.y = canvas.height - radius;
+        }
       }
   
       this.draw = function() {
@@ -65,6 +81,8 @@ function Canvas() {
         ctx.fill();
         ctx.closePath();
       }
+
+
     }
   
     function animate() {
